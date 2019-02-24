147eb4d36785c7738504b77ba57b3eefe8a898bf
Marvin Zachary <acerzach@gmail.com>
2019-02-24 23:07:34 +0300

    Created variables to get value from radio buttons and also other varries to store data

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 function check(){
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+	var question6 = document.quiz.question6.value;
+	var correct = 0;
+	var percent;
+	var total=5;
+	var wrong;
+	var comments;
